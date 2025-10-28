ec72a8a13f714b2fa9c58eb98ae094581c02c40d
Dimitrios Maslas Maslarinos (Home) <maslas@maslasbros.com>
2025-10-28 23:14:00 +0200

    1. made sure that the length calculation of the last_issues method will not generate any exception before it reaches the conditional statement.

--- the_burdock.py
+++ the_burdock.py
@@ -44,9 +44,8 @@ class TheBurdock(discord.Client):
     async def update_issues(self):
         while True:
             last_issues = self.rest.get_last_issues(self.last_issue, self.config.rest.project_id)
-            length = len(last_issues)
             
-            if last_issues and length > 0:
+            if last_issues and len(last_issues) > 0:
                 # Collects all the issues reported after the last logged issue ID.
                 self.last_issue = last_issues[-1]['id']
 
